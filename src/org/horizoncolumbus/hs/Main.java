5f38aea83a7166abef609d992a96b0d335779dc7
C4NY0N <student.c.jeffers@horizoncolumbus.org>
2021-02-04 09:14:43 -0500

    Initial commit

---
+++ src/org/horizoncolumbus/hs/Main.java
@@ -0,0 +1,19 @@
+package org.horizoncolumbus.hs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int matrix[][] = { {0,1,1},{9,2,4},{9,0,4},{1,2,5} };
+
+        for(int i = 0; i < 4; i++){
+            int sum = 0;
+            for(int j = 0; j < 3; j++){
+                sum = sum + matrix[i][j];
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(" = " + sum);
+            System.out.println();
+        }
+    }
+}
